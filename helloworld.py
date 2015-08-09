88736ae43ae74f4c30ec14594b8a14ea02106552
Misko Hevery <misko@hevery.com>
2015-08-09 07:29:25 -0700

    working version

--- helloworld.py
+++ helloworld.py
@@ -7,16 +7,23 @@ from google.appengine.ext import ndb
 from google.appengine.api import urlfetch
 
 
+class CoreTeamMember(ndb.Model):
+  username = ndb.StringProperty(indexed=True)
+
+  @classmethod
+  def forUsername(cls, username):
+    return CoreTeamMember.query().filter(CoreTeamMember.username == username).get();
+
+
 class AuthToken(ndb.Model):
   service = ndb.StringProperty(indexed=True)
-  username = ndb.StringProperty(indexed=True)
   token = ndb.StringProperty(indexed=False)
   
   @classmethod
-  def forService(cls, service, username):
-    auth = AuthToken.query().filter(AuthToken.service == service and AuthToken.username == username).get();
+  def forService(cls, service):
+    auth = AuthToken.query().filter(AuthToken.service == service).get();
     if (auth is None):
-      auth = AuthToken(service=service, username=username, token="")
+      auth = AuthToken(service=service, token="")
       auth.put()
       return None
     else:
@@ -43,22 +50,33 @@ class MainPage(webapp2.RequestHandler):
 
 
 class WebHookPage(webapp2.RequestHandler):
-  def urlMethod(self, method, url):
+  def urlMethod(self, method, url, payload):
     fullUrl = "https://api.github.com/repos/angular/angular/" + url;
+    headers =  {"Authorization": "token " + self.token}
+    if (payload != None):
+      payload = json.dumps(payload)
     self.response.out.write(method)
     self.response.out.write(' ')
     self.response.out.write(fullUrl)
     self.response.out.write(' => ')
-    response = urlfetch.Fetch(fullUrl, method = method, headers = {"Authorization": "token " + self.token})
+    response = urlfetch.Fetch(fullUrl, method = method, headers = headers, payload = payload)
     self.response.out.write(response.status_code)
     self.response.out.write('\n')
     return response
   
   def urlGET(self, url):
-    return self.urlMethod(urlfetch.GET, url);
+    return self.urlMethod(urlfetch.GET, url, None);
     
   def urlDELETE(self, url):
-    return self.urlMethod(urlfetch.DELETE, url);
+    return self.urlMethod(urlfetch.DELETE, url, None);
+    
+  def urlPOST(self, url, content):
+    return self.urlMethod(urlfetch.POST, url, content);
+    
+  def urlPATCH(self, url, content):
+    return self.urlMethod(urlfetch.PATCH, url, content);
+    
+  
   
   def get(self):
     AuthToken.forService('github')
@@ -66,6 +84,8 @@ class WebHookPage(webapp2.RequestHandler):
     self.response.out.write('Hello WebHook!')
       
   def post(self):
+    if (CoreTeamMember.forUsername('*') == None):
+      CoreTeamMember(username = '*').put()      
     event = self.request.headers["X-Github-Event"],
     if (event[0] != 'pull_request'):
       self.response.out.write('Not pull_request got ' + event[0])
@@ -76,22 +96,35 @@ class WebHookPage(webapp2.RequestHandler):
       body = self.request.body)
     audit.put()
     data = json.loads(audit.body)
-    self.token = AuthToken.forService('github', '*')
+    self.token = AuthToken.forService('github')
     if (self.token is None):
       self.response.out.write('No auth token')
       return
     pr_number = str(data['number'])
-    result = self.urlGET('issues/' + pr_number + '/events')
+    sha = data['merge_commit_sha']
+    issueUrl = 'issues/' + pr_number
+    result = self.urlGET(issueUrl + '/events')
     if result.status_code == 200:
-      actionMerge = None
+      mergeUser = None
       for e in json.loads(result.content):
-        if e['event'] == 'labeled' and e['label']['name'] == 'pr_action: merge' and e['actor']['login'] == 'mhevery':
-          actionMerge = 'mhevery'
+        if e['event'] == 'labeled' and e['label']['name'] == 'pr_action: merge':
+          mergeUser = e['actor']['login'];
         if e['event'] == 'unlabeled' and e['label']['name'] == 'pr_action: merge':
-          actionMerge = None
-      self.response.out.write('Merge action? ' + str(actionMerge) + '\n')
-      if (actionMerge != None): 
-        result = self.urlDELETE('issues/' + pr_number + '/labels/pr_action:%20merge');
+          mergeUser = None
+      if (mergeUser == None):
+        return
+      self.response.out.write('Merge action? ' + str(mergeUser) + '\n')
+      result = self.urlDELETE(issuesUrl + '/labels/pr_action:%20merge');
+      if (CoreTeamMember.forUsername(mergeUser) == None):
+        self.response.out.write(mergeUser + ' is not a core team memmber with merge privlidges.')
+        self.urlPOST(issueUrl + '/comments', {'body': 'User @' + mergeUser + ' does not have PR merging privlidges.'})
+        return
+      if (mergeUser != None): 
+        branch = 'presubmit-' + mergeUser + '-pr-' + pr_number
+        self.urlPOST(issueUrl + '/comments', {'body': 'Merging PR #' + pr_number + ' on behalf of @' + mergeUser + ' to branch [' + branch + '](https://github.com/angular/angular/tree/' + branch + ').'})
+        response = self.urlPOST('git/refs', {'ref': 'refs/heads/' + branch, 'sha': sha})
+        if (response.status_code == 422):
+          self.urlPATCH('git/refs/heads/' + branch, {'sha': sha, 'force': True})
 
 
 
